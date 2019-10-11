43e02f3444528a81e509acb1d9b2c88c1d93e83d
Joe Cellucci <celluj34@gmail.com>
2019-10-11 18:09:22 -0500

    add migration for initial table creation

---
+++ Bartendro.Database/Migrations/20191011230035_InitialMigration.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bartendro.Database.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Recipes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Version = table.Column<byte[]>(rowVersion: true, nullable: true),
+                    DateCreated = table.Column<DateTimeOffset>(nullable: false),
+                    DateModified = table.Column<DateTimeOffset>(nullable: false),
+                    Title = table.Column<string>(maxLength: 128, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Recipes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ingredients",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Version = table.Column<byte[]>(rowVersion: true, nullable: true),
+                    DateCreated = table.Column<DateTimeOffset>(nullable: false),
+                    DateModified = table.Column<DateTimeOffset>(nullable: false),
+                    Title = table.Column<string>(maxLength: 128, nullable: false),
+                    Ounces = table.Column<string>(maxLength: 128, nullable: false),
+                    RecipeId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ingredients", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Ingredients_Recipes_RecipeId",
+                        column: x => x.RecipeId,
+                        principalTable: "Recipes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ingredients_RecipeId",
+                table: "Ingredients",
+                column: "RecipeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ingredients");
+
+            migrationBuilder.DropTable(
+                name: "Recipes");
+        }
+    }
+}
--- Bartendro.Web/Startup.cs
+++ Bartendro.Web/Startup.cs
@@ -16,8 +16,6 @@ namespace Bartendro.Web
             _configuration = configuration;
         }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
@@ -25,7 +23,6 @@ namespace Bartendro.Web
             services.RegisterApplication();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if(env.IsDevelopment())
@@ -35,11 +32,8 @@ namespace Bartendro.Web
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
