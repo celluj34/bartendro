bcc67982c1ce78b32682120cf11f8255a40ed096
Joe Cellucci <celluj34@gmail.com>
2019-10-14 10:58:37 -0500

    add dispenser communication and simple page to list them

---
+++ Bartendro.Application/Orchestrators/DispensersOrchestrator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Bartendro.Application.Services;
+
+namespace Bartendro.Application.Orchestrators
+{
+    public interface IDispensersOrchestrator
+    {
+        IEnumerable<string> GetPorts();
+    }
+
+    internal class DispensersOrchestrator : IDispensersOrchestrator
+    {
+        private readonly ISerialPortService _serialPortService;
+
+        public DispensersOrchestrator(ISerialPortService serialPortService)
+        {
+            _serialPortService = serialPortService;
+        }
+
+        public IEnumerable<string> GetPorts()
+        {
+            return _serialPortService.GetPorts();
+        }
+    }
+}
---
+++ Bartendro.Application/Services/SerialPortService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.IO.Ports;
+
+namespace Bartendro.Application.Services
+{
+    internal interface ISerialPortService
+    {
+        IEnumerable<string> GetPorts();
+    }
+
+    internal class SerialPortService : ISerialPortService
+    {
+        public IEnumerable<string> GetPorts()
+        {
+            return SerialPort.GetPortNames();
+        }
+    }
+}
