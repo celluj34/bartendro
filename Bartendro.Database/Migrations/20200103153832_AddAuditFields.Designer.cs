247be3300b00d13eb5e969487d021b0944c6d766
Joseph Cellucci <jcellucci@tenaska.com>
2020-01-03 09:39:30 -0600

    add migration for Deleted column

--- Bartendro.Database/Migrations/20200103153832_AddAuditFields.Designer.cs
+++ Bartendro.Database/Migrations/20200103153832_AddAuditFields.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Bartendro.Database.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Bartendro.Database.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20200103153832_AddAuditFields")]
+    partial class AddAuditFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Bartendro.Database/Migrations/20200103153832_AddAuditFields.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bartendro.Database.Migrations
+{
+    public partial class AddAuditFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "Recipes",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "Ingredients",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "Recipes");
+
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "Ingredients");
+        }
+    }
+}
