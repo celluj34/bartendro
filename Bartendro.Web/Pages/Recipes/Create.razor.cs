f5f2461d4bc8c56b6c7e33870b3ce331bc870c7e
Joseph Cellucci <jcellucci@tenaska.com>
2020-01-07 08:52:18 -0600

    update nuget packages

--- Bartendro.Web/Pages/Recipes/Create.razor.cs
+++ Bartendro.Web/Pages/Recipes/Create.razor.cs
@@ -2,7 +2,7 @@
 using Bartendro.Database.Entities;
 using Bartendro.Database.Models;
 using Bartendro.Database.Services;
-using Blazor.Extensions.Storage;
+using Blazor.Extensions.Storage.Interfaces;
 using Microsoft.AspNetCore.Components;
 
 namespace Bartendro.Web.Pages.Recipes
@@ -21,7 +21,7 @@ namespace Bartendro.Web.Pages.Recipes
         private ICommandFactory CommandFactory {get;set;}
 
         [Inject]
-        private LocalStorage LocalStorage {get;set;}
+        private ILocalStorage LocalStorage {get;set;}
 
         private async Task HandleValidSubmit()
         {
